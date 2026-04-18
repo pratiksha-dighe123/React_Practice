752fee944da4dd1da1af6554b5406cf1200b8132
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-04-18 17:38:11 +0530

    Added new Project

---
+++ src/components/Section1/RightContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const RightContent = () => {
+  return (
+    <div className='text-white h-full w-3/4 bg-blue-400'>
+
+    </div>
+  )
+}
+
+export default RightContent
